e74ca9b1c8261820eb166fa4214fa02966b7031c
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-07-11 19:53:12 +0800

    Add flags for test-exclude

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const fs = require('fs')
 const Transformer = require('./lib/transformer')
 const definition = require('./lib/definition')
 
+function parseOrThrow (value, error) {
+  try {
+    return JSON.parse(value)
+  } catch (e) {
+    throw new Error(error)
+  }
+}
+
 async function main (args) {
   if (args.positionals.length < 1) {
     console.error('Script path not specified')
@@ -33,17 +41,21 @@ async function main (args) {
   })
 
   const reports = fs.readdirSync(tmpDir).map(file => path.join(tmpDir, file))
-    .map(file => JSON.parse(fs.readFileSync(file, 'utf8')))
+    .map(file => parseOrThrow(fs.readFileSync(file, 'utf8'), `Failed to parse coverage report at ${file}`))
     .filter(v8Report => Array.isArray(v8Report?.result))
 
   const transformer = new Transformer({
     includeRelative: args.flags.includeRelative,
-    exclude: args.flags.exclude,
     reportsDirectory: args.flags.reportsDir,
     watermarks: args.flags.watermarks,
     reporters: args.flags.reporter?.split(','),
     reporterOptions: args.flags.reporterOptions,
-    skipFull: args.flags.skipFull
+    skipFull: args.flags.skipFull,
+    exclude: args.flags.exclude && parseOrThrow(args.flags.exclude, 'Exclude option must be valid JSON'),
+    include: args.flags.include && parseOrThrow(args.flags.include, 'Include option must be valid JSON'),
+    extension: args.flags.extension && parseOrThrow(args.flags.extension, 'Extension must be valid JSON'),
+    allowExternal: args.flags.allowExternal,
+    includeNodeModules: args.flags.includeNodeModules
   })
   const coverageMap = await transformer.transformToCoverageMap(reports)
   transformer.report(coverageMap)
