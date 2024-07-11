e8a44b5baa9f5d527290fec41ec46f9c2b0e676a
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-07-12 01:09:30 +0800

    Simplify setup into one function

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+const { Session } = require('node:inspector')
+const { promisify } = require('util')
 const Transformer = require('./lib/transformer')
-const runWithCoverage = require('./lib/runner.js')
 
-module.exports = { Transformer, runWithCoverage }
+module.exports = async function setupCoverage (opts) {
+  const session = new Session()
+  session.connect()
+
+  const sessionPost = promisify(session.post).bind(session)
+
+  await sessionPost('Profiler.enable')
+  await sessionPost('Profiler.startPreciseCoverage', { callCount: true, detailed: true })
+
+  process.once('beforeExit', async () => {
+    const v8Report = await sessionPost('Profiler.takePreciseCoverage')
+    session.disconnect()
+
+    const transformer = new Transformer({ reporters: ['json', 'text'], opts })
+    const coverageMap = await transformer.transformToCoverageMap([v8Report])
+    transformer.report(coverageMap)
+  })
+}
