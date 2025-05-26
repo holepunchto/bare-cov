f221a134d5b688697b565980d1a4e89f39b3dcc4
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-05-27 03:21:09 +0800

    Rename test files to have .test in their name

--- test/index.js
+++ test/index.js
@@ -7,9 +7,9 @@ async function runTests () {
 
   test.pause()
 
-  await import('./basic.js')
-  await import('./merge.js')
-  await import('./cmd.js')
+  await import('./basic.test.js')
+  await import('./merge.test.js')
+  await import('./cmd.test.js')
 
   test.resume()
 }
